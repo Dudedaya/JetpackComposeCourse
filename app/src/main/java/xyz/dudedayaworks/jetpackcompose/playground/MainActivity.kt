d7d3f5f551a4eaabdc39dce960753190896c21de
Sergey Averchenkov <sav@tassta.com>
2023-10-31 22:15:52 +0400

    lesson 2.8

--- app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/MainActivity.kt
+++ app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/MainActivity.kt
@@ -3,22 +3,25 @@ package xyz.dudedayaworks.jetpackcompose.playground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import xyz.dudedayaworks.jetpackcompose.playground.ui.InstagramHeader
 import xyz.dudedayaworks.jetpackcompose.playground.ui.theme.JetpackComposePlaygroundTheme
 
@@ -38,36 +41,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
-fun TextsExample() {
-    Text(
-        text = "Hello World!",
-        fontSize = 24.sp,
-        fontWeight = FontWeight.Bold,
-        fontStyle = FontStyle.Italic,
-        fontFamily = FontFamily.Cursive,
-        textDecoration = TextDecoration.combine(
-            listOf(TextDecoration.Underline, TextDecoration.LineThrough)
-        ),
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun TextsExampleAnnotated() {
-    Text(
-        buildAnnotatedString {
-            withStyle(SpanStyle(Color.Red)) {
-                append("Hello")
-            }
-            withStyle(SpanStyle(textDecoration = TextDecoration.Underline)) {
-                append(" ")
-            }
-            withStyle(SpanStyle(fontStyle = FontStyle.Italic)) {
-                append("World")
-            }
-            append("!")
-        }
+fun ImageExample() {
+    Image(
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(Brush.linearGradient(colors = listOf(Color.Magenta, Color(0xFF7F00FF))))
+            .padding(8.dp)
+            .border(width = 4.dp, color = Color.White, shape = CircleShape),
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = stringResource(id = R.string.app_name),
+        colorFilter = ColorFilter.tint(color = Color.White),
+        contentScale = ContentScale.Fit,
     )
 }
--- app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/ui/InstagramHeader.kt
+++ app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/ui/InstagramHeader.kt
@@ -1,9 +1,10 @@
 package xyz.dudedayaworks.jetpackcompose.playground.ui
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,12 +19,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import xyz.dudedayaworks.jetpackcompose.playground.R
 import xyz.dudedayaworks.jetpackcompose.playground.ui.theme.JetpackComposePlaygroundTheme
 
 @Composable
@@ -44,10 +52,28 @@ fun InstagramHeader() {
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                Box(
+                Image(
                     modifier = Modifier
                         .size(48.dp)
-                        .background(color = Color.Green),
+                        .clip(RoundedCornerShape(12.dp))
+                        .background(
+                            Brush.linearGradient(
+                                colors = listOf(
+                                    Color.Magenta,
+                                    Color(0xFF7F00FF)
+                                )
+                            )
+                        )
+                        .padding(4.dp)
+                        .border(
+                            width = 3.dp,
+                            color = Color.White,
+                            shape = RoundedCornerShape(9.dp)
+                        ),
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = stringResource(id = R.string.app_name),
+                    colorFilter = ColorFilter.tint(color = Color.White),
+                    contentScale = ContentScale.Fit,
                 )
                 CounterWithText(count = "555", text = "posts")
                 CounterWithText(count = "12M", text = "followers")
