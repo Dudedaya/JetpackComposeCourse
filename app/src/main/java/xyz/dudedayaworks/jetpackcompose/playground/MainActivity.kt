6368571a0dfd72aaa3cfb3704cb5b8f6f16bc101
Sergey Averchenkov <sav@tassta.com>
2023-10-31 19:33:39 +0400

    lesson 2.5

--- app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/MainActivity.kt
+++ app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/MainActivity.kt
@@ -3,6 +3,18 @@ package xyz.dudedayaworks.jetpackcompose.playground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import xyz.dudedayaworks.jetpackcompose.playground.ui.InstagramHeader
 
 class MainActivity : ComponentActivity() {
@@ -10,6 +22,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             InstagramHeader()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CardExample() {
+    Card(
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        border = BorderStroke(width = 2.dp, color = Color.Black),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Yellow,
+            contentColor = Color.Black,
+        ),
+    ) {
+        Box(modifier = Modifier.padding(8.dp)) {
+            Text(text = "Hello card")
         }
     }
 }
--- app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/ui/InstagramHeader.kt
+++ app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/ui/InstagramHeader.kt
@@ -1,5 +1,6 @@
 package xyz.dudedayaworks.jetpackcompose.playground.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,22 +22,27 @@ import androidx.compose.ui.unit.dp
 @Preview(showBackground = true)
 @Composable
 fun InstagramHeader() {
-    Column {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Box(
+    Card(
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        border = BorderStroke(width = 1.dp, color = Color.Black),
+    ) {
+        Column {
+            Row(
                 modifier = Modifier
-                    .size(48.dp)
-                    .background(color = Color.Green),
-            )
-            CounterWithText()
-            CounterWithText()
-            CounterWithText()
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .background(color = Color.Green),
+                )
+                CounterWithText()
+                CounterWithText()
+                CounterWithText()
+            }
         }
     }
 }
