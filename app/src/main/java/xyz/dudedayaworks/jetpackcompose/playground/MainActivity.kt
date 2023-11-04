50a89df773cb3f2718510133d41393d987dafa73
Sergey Averchenkov <sav@tassta.com>
2023-11-04 11:27:31 +0400

    lesson 4.6

--- app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/MainActivity.kt
+++ app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import xyz.dudedayaworks.jetpackcompose.playground.ui.InstagramHeader
 import xyz.dudedayaworks.jetpackcompose.playground.ui.MainViewModel
@@ -17,11 +19,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         val viewModel by viewModels<MainViewModel>()
         setContent {
-            JetpackComposePlaygroundTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.surface,
-                ) {
+            CardListTest(viewModel = viewModel)
+        }
+    }
+}
+
+@Composable
+private fun CardListTest(viewModel: MainViewModel) {
+    JetpackComposePlaygroundTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.surface,
+        ) {
+            LazyColumn {
+//                repeat(100) {
+//                    item {
+//                        if (it == 0) return@item
+//                        Text(
+//                            text = when {
+//                                (it % 15 == 0) -> "FizzBuzz"
+//                                (it % 5 == 0) -> "Buzz"
+//                                (it % 3 == 0) -> "Fizz"
+//                                else -> it.toString()
+//                            }
+//                        )
+//                    }
+//                }
+
+                items(500) {
                     InstagramHeader(viewModel)
                 }
             }
