fa419caf3b4298c17faa9223960060d2a47c2ad7
Sergey Averchenkov <sav@tassta.com>
2023-11-07 21:36:03 +0400

    lesson_5.3

--- app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/ui/NewsMainScreen.kt
+++ app/src/main/java/xyz/dudedayaworks/jetpackcompose/playground/ui/NewsMainScreen.kt
@@ -1,5 +1,8 @@
 package xyz.dudedayaworks.jetpackcompose.playground.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -10,10 +13,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import xyz.dudedayaworks.jetpackcompose.playground.navigation.AppNavGraph
+import xyz.dudedayaworks.jetpackcompose.playground.navigation.Screen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,7 +41,15 @@ fun MainScreen(viewModel: MainViewModel) {
             MainBottomBar(
                 currentRoute = currentRoute,
                 navigationItems = navigationItems,
-                onNavItemSelected = { navHostController.navigate(it.screen.route) },
+                onNavItemSelected = {
+                    navHostController.navigate(it.screen.route) {
+                        launchSingleTop = true
+                        popUpTo(Screen.NewsFeed.route) {
+                            saveState = true
+                        }
+                        restoreState = true
+                    }
+                },
             )
         }
     ) { paddingValues ->
@@ -43,8 +61,26 @@ fun MainScreen(viewModel: MainViewModel) {
                     paddingValues = paddingValues,
                 )
             },
-            favoriteScreenContent = { Text(text = "Favorites") },
-            profileScreenContent = { Text(text = "Profile") })
+            favoriteScreenContent = { StatefulText(text = "Favorites") },
+            profileScreenContent = { StatefulText(text = "Profile") })
+    }
+}
+
+@Composable
+private fun StatefulText(text: String) {
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable { count++ },
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = "$text count:$count",
+            fontSize = 30.sp,
+        )
     }
 }
 
